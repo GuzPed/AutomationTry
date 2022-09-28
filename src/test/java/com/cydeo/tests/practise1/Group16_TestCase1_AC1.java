b7e9beb9d9e58046d1ceae86e5d59ec5b80c3a11
guzel <guzel.khisamieva89@gmail.com>
2022-09-27 22:18:02 -0400

    US16-1_AC1_AC2 login function

---
+++ src/test/java/com/cydeo/tests/practise1/Group16_TestCase1_AC1.java
@@ -0,0 +1,78 @@
+package com.cydeo.tests.practise1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Group16_TestCase1_AC1 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        driver.get("https://login1.nextbasecrm.com/");
+
+        List<String> login = new ArrayList<>(Arrays.asList("hr46@cydeo.com", "hr47@cydeo.com", "hr48@cydeo.com", "helpdesk46@cydeo.com", "helpdesk47@cydeo.com", "helpdesk48@cydeo.com", "marketing46@cydeo.com", "marketing47@cydeo.com", "marketing48@cydeo.com"));
+        String password = "UserUser";
+
+        String expextedTitle = "Portal";
+
+
+        for(int i=0; i< login.size(); i++){
+
+            //finds login input and paste the value
+            WebElement loginInput = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+            //clears the login input, because after loging out, the value stays from previous step
+            loginInput.clear();
+            loginInput.sendKeys(login.get(i));
+
+            //finds password input and paste the value
+            WebElement passwordInput = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+            passwordInput.sendKeys(password);
+
+            //find login button and clicks it
+            WebElement loginButton = driver.findElement(By.xpath("//input[@class='login-btn']"));
+            loginButton.click();
+
+            //gets the actual title of the page
+            String actualTitle = driver.getTitle();
+
+            //checks if it logged in or not
+            if (actualTitle.equals(expextedTitle)){
+                System.out.println("Username " + login.get(i) + ", password " + password + " login verification passed");
+            } else {
+                System.out.println("Username " + login.get(i) + ", password " + password + " login verification FAILED");
+            }
+
+            //find the profile button of options to logout
+            WebElement profileButton = driver.findElement(By.xpath("//div[@id='user-block']"));
+            profileButton.click();
+
+            //finds log out button and clicks it
+            WebElement logoutButton = driver.findElement(By.xpath("//a[@class='menu-popup-item menu-popup-no-icon '][3]/span[2]"));
+            logoutButton.click();
+
+            //refreshes page (login page)
+            driver.navigate().refresh();
+
+           //loginInput.clear();
+
+
+        }
+
+
+        driver.quit();
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/tests/practise1/Group16_TestCase1_AC2.java
@@ -0,0 +1,63 @@
+package com.cydeo.tests.practise1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Group16_TestCase1_AC2 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        driver.get("https://login1.nextbasecrm.com/");
+
+     //   List<String> login = new ArrayList<>(Arrays.asList("hr46@Cydeo.com", "hr47@cydeo.com", "hr48@cydeo.com", "helpdesk46@cydeo.com", "helpdesk47@cydeo.com", "helpdesk48@cydeo.com", "marketing46@cydeo.com", "marketing47@cydeo.com", "marketing48@cydeo.com"));
+       String expectedErrorMessage = "Incorrect username or password";
+
+        Map<String, String> credentials= new HashMap<>(Map.of("hr46@cydeo.com", "USERUSER", "helpdesk47@cydeo.com", "useruser", "marketing4666@CYDEO.com", "UserUser"));
+
+        for(Map.Entry<String, String> eachEntry: credentials.entrySet()){
+
+            //finds login input and paste the value
+            WebElement loginInput = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+            //clears the login input, because after loging out, the value stays from previous step
+            loginInput.clear();
+            loginInput.sendKeys(eachEntry.getKey());
+
+            //finds password input and paste the value
+            WebElement passwordInput = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+            passwordInput.sendKeys(eachEntry.getValue());
+
+            //find login button and clicks it
+            WebElement loginButton = driver.findElement(By.xpath("//input[@class='login-btn']"));
+            loginButton.click();
+
+            String actualErrorText = driver.findElement(By.xpath("//div[@class='errortext']")).getText();
+            System.out.println(actualErrorText);
+            System.out.println(expectedErrorMessage);
+
+            if(actualErrorText.equals(expectedErrorMessage)){
+                System.out.println("Username " + eachEntry.getValue() + ", password " + eachEntry.getValue() + ". INVALID login verification passed");
+            } else {
+                System.out.println("Username " + eachEntry.getValue() + ", password " + eachEntry.getValue() + ". INVALID login verification FAILED");
+            }
+
+
+            //refreshes page (login page)
+            driver.get("https://login1.nextbasecrm.com/");
+
+        }
+
+
+        driver.quit();
+
+
+    }
+}
